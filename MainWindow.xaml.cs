13cca011bc11a139870840391067aff1b9002d49
Paddy0703 <132016072+Paddy0703@users.noreply.github.com>
2026-02-23 20:31:02 +0100

    Refactor MainWindow constructor to initialize database
    Removed message box display and data loading logic.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,16 +26,7 @@ namespace ITS_MaxTemp
         {
             InitializeComponent();
             tempData = new TemperatureData(@"C:\Users\Patrick\Documents\Gruppenprojekt\ITS_MaxTemp\Data", "temps.csv");
-            MessageBox.Show($"Anzahl Zeilen: {tempData.RawData.Count}");
-
-            if (tempData.RawData.Count > 0)
-            {
-                testLabel.Content = tempData.RawData[0];
-            }
-            else
-            {
-                testLabel.Content = "Keine Daten geladen!";
-            }
+            DataAccess.InitializeDatabase();
 
         }
     }
