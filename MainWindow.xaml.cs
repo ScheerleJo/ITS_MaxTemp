20fb5bef165ed7fb9d25690c2923618ad26b8b2a
Paddy0703 <PatrickDobler@t-online.de>
2026-02-23 22:10:23 +0100

    Delete emrge markers

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,13 +25,8 @@ namespace ITS_MaxTemp
         public MainWindow()
         {
             InitializeComponent();
-<<<<<<< HEAD
             tempData = new TemperatureData(@"C:\Users\Patrick\Documents\Gruppenprojekt\ITS_MaxTemp\Data", "temps.csv");
             DataAccess.InitializeDatabase();
-=======
-            //tempData = new TemperatureData(@".\Data", "temps.csv");
-            //DataAccess.InitializeDatabase();
->>>>>>> 6fe337be58cb4ca9a3bc1e7b175dece81bce70c3
 
         }
     }
