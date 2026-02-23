37a3ef8d89807d698df8039d5748e6b9bd2287a9
unknown <Paddy0703>
2026-02-23 21:08:43 +0100

    Cleaning Repo and adding DataSet

--- Models/DataSet.cs
+++ Models/DataSet.cs
@@ -8,5 +8,19 @@ namespace ITS_MaxTemp.Models
 {
     internal class DataSet
     {
+        private string sensor;
+        private DateTime time;
+        private double temperature;
+
+        public string Sensor { get => sensor; }
+        public DateTime Time { get => time; }
+        public double Temperature { get => temperature; }
+
+        public DataSet(string sensor, DateTime time, double temperature)
+        {
+            this.sensor = sensor;
+            this.time = time;
+            this.temperature = temperature;
+        }
     }
 }
