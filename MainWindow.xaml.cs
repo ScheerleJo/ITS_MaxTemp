fa9cd63e70193b6eb8161b36832ca48fc239bf15
ScheerleJo <scheerle.josia@gmail.com>
2026-02-24 20:04:26 +0100

    add SQL functionality and make CB fill dynamically

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,48 +23,79 @@ namespace ITS_MaxTemp
     public partial class MainWindow : Window
     {
         TemperatureData tempData;
+
         string path = "";
         public MainWindow()
         {
-            InitializeComponent();     
+            InitializeComponent();
             DataAccess.InitializeDatabase();
+
+            //Add sensor names to ComboBox, if Data is already available
+            AddSensorsToComboBox();
         }
 
         private void setFilePath() {
             if (path == "")
             {
-                tempData = new TemperatureData(@".\Data", "temps.csv"); // TODO: Reagieren auf bereits reingeladene File, sonst überladen´ständig unsere Datenbank mit den gleichen Daten
+                tempData = new TemperatureData(@".\Data", "temps.csv");
                 return;
             }
             FilePath.Text = path;
             tempData = new TemperatureData(path);
         }
+
         private void UploadFile_Click(object sender, RoutedEventArgs e)
         {
-           OpenFileDialog openFileDialog = new OpenFileDialog(){
-               Filter = "CSV files (*.csv)|*.csv"
-           };
+            OpenFileDialog openFileDialog = new OpenFileDialog(){
+                Filter = "CSV files (*.csv)|*.csv"
+            };
 
             if(openFileDialog.ShowDialog() == true)
             {
                 path = openFileDialog.FileName;
             }
+            setFilePath();
+
+            //Add Data to SQLite DB
+            DataAccess.clearTable(); // clear table -> no duplicates
+            DataAccess.AddData(tempData);
+
+            AddSensorsToComboBox();
         }
 
         private void EvaluateDataClick(object sender, RoutedEventArgs e)
         {
-            setFilePath(); //-> Muss in dieser Funktion aufgerufen werden, verschiebe es nach belieben 
-            string selectedSensor = (SensorComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+            if(DataAccess.checkIfTableEmpty())
+            {
+                MessageBox.Show("No data available. Please upload a CSV file first.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            string selectedSensor = SensorComboBox.SelectedItem as string;
             DateTime? fromDate = FromDatePicker.SelectedDate;
             DateTime? toDate = ToDatePicker.SelectedDate;
 
             Console.WriteLine($"Sensor: {selectedSensor}\nFrom: {fromDate}\nTo: {toDate}", "Evaluate Data");
-            // Filterung fuer Josia von Sensor und Zeit, also bitte noch hier Funktionen implementieren mit DB
-            // -> Also auswerten button muss halt gemacht werden (sql statement mit den werten aus den vars von oben)
+            float? sensorValue;
+            DateTime? dateValue;
 
-            TemperatureTextBlock.Text = "XXXXX";
-            DateTextBlock.Text = "XXXXX";
+            if (fromDate.HasValue && toDate.HasValue)
+            {
+                (sensorValue, dateValue) = DataAccess.getMaxSensorValue(selectedSensor, fromDate.Value, toDate.Value);
+            }
+            else
+            {
+                (sensorValue, dateValue) = DataAccess.getMaxSensorValue(selectedSensor);
+            }
+
+            TemperatureTextBlock.Text = $"{sensorValue} °C";
+            DateTextBlock.Text = dateValue.ToString();
         }
 
+        private void AddSensorsToComboBox()
+        {
+            SensorComboBox.Items.Clear(); // clear items, to add actual sensors
+            SensorComboBox.ItemsSource = DataAccess.getSensorNames();
+            SensorComboBox.SelectedIndex = 0;
+        }
     }
 }
